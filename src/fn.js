69fd846a059c7da5b06a02ea0814f3ebb157b098
Bartosz Legięć <bartosz@legiec.eu>
2019-12-06 23:20:36 +0100

    Add challenge

--- src/fn.js
+++ src/fn.js
@@ -1,4 +1,56 @@
-const fn = () => {};
+const fn = () => {
+  const farmerFavouriteAnimals = [
+    {
+      emoji: "🐮",
+      foodMadeFromAnimal: [
+        '🍔'
+      ]
+    },
+    {
+      emoji: "🐓",
+      foodMadeFromAnimal: [
+        '🍗'
+      ]
+    },
+    {
+      emoji: "🐟",
+      foodMadeFromAnimal: [
+        '🍣'
+      ]
+    },
+    {
+      emoji: "🐟",
+      foodMadeFromAnimal: [
+        '🍣'
+      ]
+    },
+    {
+      emoji: "🐟",
+      foodMadeFromAnimal: [
+        '🍣'
+      ]
+    },
+  ]
+
+  const numberOfAnimalsAsString = ['1', '8', '0', '20', '13'];
+  const numberOfAnimals = numberOfAnimalsAsString
+    .map(Number.parseInt);
+
+  const numberOfAnimalsSorted = numberOfAnimals.sort();
+
+  const foodFromAnimals = numberOfAnimalsSorted
+    .map((numberOfAnimals, i) => {
+      const animal = farmerFavouriteAnimals[i];
+      return `From ${animal.emoji}: ${String(numberOfAnimals).padStart(2, '0')} x ${animal.foodMadeFromAnimal[0]}`
+    });
+
+  const numberOfCows = numberOfAnimals[0];
+
+  return {
+    foodFromAnimals,
+    numberOfCows
+  };
+}
 
 module.exports = {
   fn,
--- src/fn.test.js
+++ src/fn.test.js
@@ -6,4 +6,20 @@ describe('fn', () => {
   it('should not return fn as undefined', () => {
     expect(fn).not.toBeUndefined();
   });
+
+  it('should animals sorted by food', async () => {
+    expect(await fn().foodFromAnimals).toEqual(
+      [
+        'From 🐮: 00 x 🍔',
+        'From 🐓: 01 x 🍗',
+        'From 🐟: 08 x 🍣',
+        'From 🐟: 13 x 🍣',
+        'From 🐟: 20 x 🍣',
+      ]
+    );
+  });
+
+  it('should return number of cows', async () => {
+    expect(await fn().numberOfCows).toBe(1);
+  });
 });
